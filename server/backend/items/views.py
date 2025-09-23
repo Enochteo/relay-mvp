b37358c3dc0f2672c1c47d69ff7aeb43eeecb42a
Enochteo <enochowoade@gmail.com>
2025-09-23 01:10:59 -0500

    add item endpoints

--- server/backend/items/views.py
+++ server/backend/items/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
 from .models import Item
 from .serializers import ItemSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 # Create your views here.
 class ItemViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,9 @@ class ItemViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(seller=self.request.user)
+    
+    @action(detail=False, methods=["get"], permission_classes=[permissions.IsAuthenticated])
+    def my_items(self, request):
+        items = Item.objects.filter(seller=request.user).order_by("-created_at")
+        serializer = self.get_serializer(items, many=True)
+        return Response(serializer.data)
