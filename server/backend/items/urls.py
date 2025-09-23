18759d03ae80121832615186fef06ef33ddef1ee
Enochteo <enochowoade@gmail.com>
2025-09-23 01:10:35 -0500

    add item endpoints

--- server/backend/items/urls.py
+++ server/backend/items/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from .views import ItemViewSet
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet, basename='item')
 
-urlpatterns = router.urls
+urlpatterns = [
+    # ... other urls
+    path("api/", include(router.urls)),
+]
