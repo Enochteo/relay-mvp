4958a44d035382c9499da0423077de318ab5b9c1
Enochteo <enochowoade@gmail.com>
2025-09-23 02:35:15 -0500

    add edit and view item routes

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -8,7 +8,8 @@ import Browse from "./pages/Browse";
 import PostItem from "./pages/PostItem";
 import VerifyEmail from "./pages/auth/VerifyEmail";
 import ProtectedRoute from "./components/ProtectedRoute";
-
+import ItemDetail from "./pages/items/ItemDetail";
+import EditItem from "./pages/items/EditItem";
 
 
 function App() {
@@ -22,6 +23,8 @@ function App() {
         <Route path="/verify/:token" element={<VerifyEmail />} />
         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/post" element={<ProtectedRoute><PostItem /></ProtectedRoute>} />
+        <Route path="/items/:id" element={<ItemDetail />} />
+        <Route path="/items/:id/edit" element={<EditItem />} />
       </Routes>
     </Router>
     )
