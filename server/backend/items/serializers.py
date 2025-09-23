7ee29a4ead0e48ffa6ecaef10134f62e1ab04cf4
Enochteo <enochowoade@gmail.com>
2025-09-23 02:36:35 -0500

    add correct backend settings for image parsing and storing

--- server/backend/items/serializers.py
+++ server/backend/items/serializers.py
@@ -21,7 +21,7 @@ class ItemImageSerializer(serializers.ModelSerializer):
 
 class ItemSerializer(serializers.ModelSerializer):
     seller = SellerSerializer(read_only=True)
-    images = ItemImageSerializer(many=True, read_only=True)
+    images = ItemImageSerializer(many=True, required=False)
 
     class Meta:
         model = Item
@@ -33,6 +33,22 @@ class ItemSerializer(serializers.ModelSerializer):
         read_only_fields = ["id", "created_at", "seller"]
 
     def create(self, validated_data):
-        #seller = self.context["request"].user
+        request = self.context.get("request")
+        images_data = request.FILES.getlist("images")
         item = Item.objects.create(**validated_data)
+        for img in images_data:
+            ItemImage.objects.create(item=item, image=img)
         return item
+    
+    def update(self, instance, validated_data):
+        images_data = self.context.get("request").FILES.getlist("images")
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if images_data:
+            # Replace old images if new ones uploaded
+            instance.images.all().delete()
+            for img in images_data:
+                ItemImage.objects.create(item=instance, image=img)
+        return instance
--- server/backend/items/views.py
+++ server/backend/items/views.py
@@ -3,12 +3,24 @@ from .models import Item
 from .serializers import ItemSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser, FormParser
 
 # Create your views here.
+class IsSellerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.seller == request.user
+    
 class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all().order_by("-created_at")
+    parser_classes = [MultiPartParser, FormParser]
+    
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsSellerOrReadOnly]
+    def get_queryset(self):
+        if self.action == "my_items":
+            return Item.objects.filter(seller=self.request.user)
+        return Item.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(seller=self.request.user)
@@ -18,3 +30,4 @@ class ItemViewSet(viewsets.ModelViewSet):
         items = Item.objects.filter(seller=request.user).order_by("-created_at")
         serializer = self.get_serializer(items, many=True)
         return Response(serializer.data)
+    
