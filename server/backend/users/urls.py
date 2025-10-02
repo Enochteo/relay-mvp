1c437fccd08b4e31fca2c08b4deff58cc5dc7720
Enochteo <enochowoade@gmail.com>
2025-10-02 02:36:49 -0500

    fix tautology in url endpoints

--- server/backend/users/urls.py
+++ server/backend/users/urls.py
@@ -4,11 +4,13 @@ from .views import (
     VerifyEmailView, LogoutView, RateProfileView
 )
 
+
 urlpatterns = [
     path("me/", MyProfileView.as_view(), name="my-profile"),
     path("register/", RegisterView.as_view(), name="custom-register"),
     path("verify-email/<str:token>/", VerifyEmailView.as_view(), name="verify-email"),
-    path("api/logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),   # <-- fixed
     path("profile/me/", MyProfileView.as_view(), name="my-profile"),
     path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
-    path("profile/<int:pk>/rate/", RateProfileView.as_view(), name="profile-rate"),]
+    path("profile/<int:pk>/rate/", RateProfileView.as_view(), name="profile-rate"),
+]
